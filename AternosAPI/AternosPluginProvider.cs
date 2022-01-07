3a9a52fd5bf6d87efa44a825d344b831f359a0bd
Dexuby <PxLz@web.de>
2022-01-07 20:49:11 +0100

    Added bukkit plugin provider.

--- AternosAPI/AternosPluginProvider.cs
+++ AternosAPI/AternosPluginProvider.cs
@@ -9,6 +9,7 @@ namespace AternosAPI
     public class AternosPluginProvider : ValueHolder<string>
     {
         public static AternosPluginProvider Spigot = new("spigot");
+        public static AternosPluginProvider Bukkit = new("bukkit");
 
         private AternosPluginProvider(string value) : base(value)
         {
