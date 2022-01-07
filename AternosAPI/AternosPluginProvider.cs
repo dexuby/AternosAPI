4d7081d40a11e969f7b49c9da5b5f288e8bf2d48
Dexuby <PxLz@web.de>
2022-01-07 20:51:51 +0100

    Added artemis bukkit plugin provider.

--- AternosAPI/AternosPluginProvider.cs
+++ AternosAPI/AternosPluginProvider.cs
@@ -10,6 +10,7 @@ namespace AternosAPI
     {
         public static AternosPluginProvider Spigot = new("spigot");
         public static AternosPluginProvider Bukkit = new("bukkit");
+        public static AternosPluginProvider ArtemisBukkit = new("artemis-bukkit");
 
         private AternosPluginProvider(string value) : base(value)
         {
